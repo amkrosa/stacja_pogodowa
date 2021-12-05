b1ef52f140050955ea2277d6499a2992bdfa68db
Anna Krasowska <annakrasowska2@gmail.com>
2021-12-05 01:18:01 +0100

    scopes?

--- src/main/java/pl/edu/pk/it/station/interfaces/temperature/controller/TemperatureController.java
+++ src/main/java/pl/edu/pk/it/station/interfaces/temperature/controller/TemperatureController.java
@@ -1,6 +1,7 @@
 package pl.edu.pk.it.station.interfaces.temperature.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +19,7 @@ public class TemperatureController {
     private final TemperatureDtoMapper temperatureDtoMapper;
 
     @RequestMapping(value = "/temperatures", method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('SCOPE_api')")
     public TemperatureListDto allTemperatures() {
         var temperatures = temperatureService.getAll();
         var dto = new TemperatureListDto();
@@ -26,6 +28,7 @@ public class TemperatureController {
     }
 
     @RequestMapping(value = "/temperatures", consumes = "application/json", method = RequestMethod.POST)
+    @PreAuthorize("hasAuthority('SCOPE_api')")
     public TemperatureListDto insertTemperatures(@RequestBody TemperatureListDto temperatureListDto) {
         var request = temperatureDtoMapper.toTemperatureList(temperatureListDto.getItemList());
         temperatureService.saveAll(request);
