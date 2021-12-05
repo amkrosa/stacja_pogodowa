5bf407ee0bd7f2f6d0c4a3862846d297c51de356
Anna Krasowska <annakrasowska2@gmail.com>
2021-12-05 01:38:28 +0100

    another security change

--- src/main/java/pl/edu/pk/it/station/infrastructure/configuration/SecurityConfig.java
+++ src/main/java/pl/edu/pk/it/station/infrastructure/configuration/SecurityConfig.java
@@ -2,9 +2,10 @@ package pl.edu.pk.it.station.infrastructure.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
