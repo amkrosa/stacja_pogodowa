405cc9813d46890d6e4db65825eb2d4adefb5bcf
Anna Krasowska <annakrasowska2@gmail.com>
2021-12-17 22:22:23 +0100

    rudimentary exception handling for POST

--- src/main/java/pl/edu/pk/it/station/interfaces/temperature/controller/TemperatureController.java
+++ src/main/java/pl/edu/pk/it/station/interfaces/temperature/controller/TemperatureController.java
@@ -1,11 +1,14 @@
 package pl.edu.pk.it.station.interfaces.temperature.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import pl.edu.pk.it.station.domain.temperature.service.TemperatureService;
 import pl.edu.pk.it.station.interfaces.temperature.dto.TemperatureListDto;
 import pl.edu.pk.it.station.interfaces.temperature.mapper.TemperatureDtoMapper;
@@ -28,11 +31,15 @@ public class TemperatureController {
 
     @RequestMapping(value = "/temperatures", consumes = "application/json", method = RequestMethod.POST)
     public TemperatureListDto insertTemperatures(@RequestBody TemperatureListDto temperatureListDto) {
-        var request = temperatureDtoMapper.toTemperatureList(temperatureListDto.getItemList());
-        temperatureService.saveAll(request);
-        var responseList = temperatureDtoMapper.toDtoList(temperatureService.getLast(temperatureListDto.getItemList().size()));
-        var response = new TemperatureListDto();
-        response.setItemList(responseList);
-        return response;
+        try {
+            var request = temperatureDtoMapper.toTemperatureList(temperatureListDto.getItemList());
+            temperatureService.saveAll(request);
+            var responseList = temperatureDtoMapper.toDtoList(temperatureService.getLast(temperatureListDto.getItemList().size()));
+            var response = new TemperatureListDto();
+            response.setItemList(responseList);
+            return response;
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage(), ex);
+        }
     }
 }
