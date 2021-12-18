00dab984f455288feb75c214f34ced7dd9d26c66
Anna Krasowska <annakrasowska2@gmail.com>
2021-12-18 12:28:49 +0100

    error messages turned on, @NotNull validation added

--- src/main/java/pl/edu/pk/it/station/interfaces/temperature/dto/TemperatureDto.java
+++ src/main/java/pl/edu/pk/it/station/interfaces/temperature/dto/TemperatureDto.java
@@ -13,13 +13,16 @@ import java.time.LocalTime;
 @Data
 public class TemperatureDto implements Serializable {
     @NotBlank(message = "Value field cannot be empty")
+    @NotNull(message = "Value field cannot be null")
     private double value;
 
     @JsonFormat(pattern = "HH:mm:ss")
     @NotBlank(message = "Time field cannot be empty")
+    @NotNull(message = "Time field cannot be null")
     private LocalTime time;
 
     @JsonFormat(pattern = "yyyy-MM-dd")
     @NotBlank(message = "Date field cannot be empty")
+    @NotNull(message = "Date field cannot be null")
     private LocalDate date;
 }
